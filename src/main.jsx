7d80933e3212f1885081437e29b4bbbf30d8f080
Luyen <luyenlaluot@gmail.com>
2026-05-29 14:55:26 +0700

    Cài đặt và cấu hình Router | Xây dựng cơ chế tải Layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "@/App";
 import GlobalStyles from "@/components/GlobalStyles";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyles>
